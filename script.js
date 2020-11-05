4d46c21de4e911ce5842b85c8316f25fbd767893
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2020-11-05 17:12:50 +0100

    CSS and HTML for dropdown menu done
    HTML and CSS is done for the dropdown menu. Started with the JavaScript but had to close down.

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-//Global ariables//
+//Global variables//
 const navbar = document.getElementById("navigation-bar");
+const dropdownMenu = document.querySelector(".dropdown-menu")
+const hamburgerButton = document.querySelector(".hamburger")
 let stickTheNavbar = navbar.offsetTop;
+
 
 
 //Eventlisteners//
 window.addEventListener("scroll", stickNavOnScroll);
+hamburgerButton.addEventListener("click", toggleHamburger)
 
 
 
 function stickNavOnScroll() {
+    
     if (window.pageYOffset >= stickTheNavbar) {
         navbar.classList.add("stickTheNavbar")
+        navbar.style.position = "fixed";
     } else {
         navbar.classList.remove("stickTheNavbar");
+        navbar.style.position = "relative";
     }
+}
+
+//MAKE THE HAMBURGER ICON CHANGE TO A CLOSE ICON
+function toggleHamburger() {
+    dropdownMenu.classList.toggle("show-dropdown")
+    
 }
