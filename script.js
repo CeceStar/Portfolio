9a916591f09c1cafcd7fc3f77cb8455cf14bad3b
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2020-11-06 12:08:17 +0100

    Dropdown menu done!
    Just finished the dropdown menu. Its a bit stiff, hope there will be time to add animations!

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 //Global variables//
 const navbar = document.getElementById("navigation-bar");
 const dropdownMenu = document.querySelector(".dropdown-menu")
+const pageLink = document.querySelectorAll(".menu-link")
+
 const hamburgerButton = document.querySelector(".hamburger")
 let stickTheNavbar = navbar.offsetTop;
 
 
-
 //Eventlisteners//
 window.addEventListener("scroll", stickNavOnScroll);
-hamburgerButton.addEventListener("click", toggleHamburger)
+hamburgerButton.addEventListener("click", toggleDropdown);
+pageLink.forEach (function(pageLink) {
+        pageLink.addEventListener("click", toggleDropdown)
+    }
+)
 
-
+//Navigation bar and Dropdownmenu
 
 function stickNavOnScroll() {
     
@@ -23,12 +28,12 @@ function stickNavOnScroll() {
     }
 }
 
-//MAKE THE HAMBURGER ICON CHANGE TO A CLOSE ICON
-function toggleHamburger() {
+function toggleDropdown() {
     dropdownMenu.classList.toggle("show-dropdown");
+
     let iconClass = hamburgerButton.children[0].className
     if (iconClass === "fas fa-bars") {
-        hamburgerButton.children[0].className =  "fas fa-times";
+        iconClass = hamburgerButton.children[0].className =  "fas fa-times";
     } else {
     hamburgerButton.children[0].className =  "fas fa-bars" ;
     }
